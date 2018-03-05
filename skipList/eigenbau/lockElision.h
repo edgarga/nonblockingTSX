2134f8c2ee149a2a680b49f60f27ed7d14b5cf49
edgar <edgar.garmel@student.uni-augsburg.de>
2018-03-05 01:05:56 +0100

    tsx versuche implementiert

---
+++ skipList/eigenbau/lockElision.h
@@ -0,0 +1,20 @@
+//
+// Created by edgar on 10.01.18.
+//
+
+#include <immintrin.h>
+
+#ifndef MUTEX_LOCKELISION_H
+#define MUTEX_LOCKELISION_H
+
+
+class LockElision {
+public:
+    unsigned startTransaction();
+    bool testTransaction();
+    bool endTransaction();
+    ~LockElision();
+};
+
+
+#endif //MUTEX_LOCKELISION_H
